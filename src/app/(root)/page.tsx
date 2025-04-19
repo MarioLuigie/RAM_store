276cae532f5f569b04a1aff8aa21c042a61a7c3f
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-19 09:19:59 +0200

    Feat: Create HomePage.tsx and import in /(root)/page.tsx

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,14 +1,11 @@
-import { Button } from '@/components/ui/button'
-import { APP_ROUTE_NAME_HOME } from '@/lib/constants';
+// lib
+import { APP_ROUTE_NAME_HOME } from '@/lib/constants'
+import HomePage from '@/components/pages/HomePage'
 
 export const metadata = {
-  title: APP_ROUTE_NAME_HOME
+	title: APP_ROUTE_NAME_HOME,
 }
 
 export default function Page() {
-  return (
-    <div>
-      <Button>HOME</Button>
-    </div>
-  );
+	return <HomePage />
 }
---
+++ src/components/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+// components
+import { Button } from '@/components/ui/button'
+
+export default function HomePage() {
+	return (
+		<div>
+			<Button>HOME</Button>
+		</div>
+	)
+}

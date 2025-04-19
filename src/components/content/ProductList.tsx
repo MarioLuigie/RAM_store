a5f04a9d4c084d3af5fa82e87bbc0ae0cbdc81e0
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-19 20:14:11 +0200

    Styles: Add grid cols with breakpoints to ProductList.tsx

--- src/components/content/ProductList.tsx
+++ src/components/content/ProductList.tsx
@@ -5,14 +5,14 @@ export default function ProductList({ data }: { data: Product[] }) {
 	return (
 		<>
 			{numberOfProducts > 0 ? (
-				<div>
+				<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
 					{data.map((product, i) => (
-						<div key={i}>{product.name}</div>
+						<div key={i} className='bg-slate-300'>{product.name}</div>
 					))}
 				</div>
 			) : (
 				<div>
-					<p>No products...</p>
+					<p>There are currently no products. Take a look a few moments later.</p>
 				</div>
 			)}
 		</>

a82ca414814e2f12e2287c5c42fbfc35dfa5fdc0
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-19 03:15:07 +0200

    Feat: Create env variables and add to title and desc constants in /lib/constants/index.ts

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,2 +1,2 @@
-export const APP_NAME = 'RAM_fashion-store'
-export const APP_DESC = 'RAM. Interpret as you wish.'
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'RAM'
+export const APP_DESC = process.env.NEXT_PUBLIC_APP_DESC || 'RAM. Interpret as you wish.'

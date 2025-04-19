6b0e4e014468da72f5617e6d682ca0a3a635808e
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-19 17:03:53 +0200

    Styles: Add logo and texts to NotFoundPage.tsx

--- src/components/pages/NotFoundPage.tsx
+++ src/components/pages/NotFoundPage.tsx
@@ -1,8 +1,26 @@
-export default function NotFoundPage() {
+// lib
+import { ROUTES } from '@/lib/constants/paths'
+// components
+import Logo from '@/components/shared/Logo'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
-  return (
-    <div className="flex-center w-screen h-screen">
-      NOT FOUND
-    </div>
-  )
+export default function NotFoundPage() {
+	return (
+		<div className="flex-center w-screen h-screen">
+			<div className='flex flex-col items-center'>
+				<Logo />
+
+				<div className="mt-6 p-6 rounded-lg shadow-md text-center">
+					<h1 className="text-3xl font-bold mb-4">Not Found</h1>
+					<p className="text-[#383638]">
+						Could not find requested resource
+					</p>
+					<Button variant="outline" className="mt-6">
+						<Link href={ROUTES.HOME}>Back to Home</Link>
+					</Button>
+				</div>
+			</div>
+		</div>
+	)
 }

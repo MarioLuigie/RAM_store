3359f1fb3517fa313f71c2800b020732ef789151
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-19 15:20:43 +0200

    Feat: Add state and effect to conditional logo display in Logo.tsx

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,4 +1,8 @@
-//lib
+'use client'
+// modules
+import { useEffect, useState } from 'react'
+import { useTheme } from 'next-themes'
+// lib
 import { APP_NAME } from '@/lib/constants'
 import { ICONS } from '@/lib/constants/icons'
 import { ROUTES } from '@/lib/constants/paths'
@@ -7,21 +11,30 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 export default function Logo() {
+	const { theme, systemTheme } = useTheme()
+	const [mounted, setMounted] = useState<boolean>(false)
 
-  return (
-    <div className="flex-start">
-    <Link href={ROUTES.HOME} className="flex-start">
-      <Image
-        src={ICONS.LOGO_LIGHT.path}
-        alt={ICONS.LOGO_LIGHT.alt}
-        width={48}
-        height={48}
-        priority
-      />
-      <span className="hidden sm:block ml-3 text-2xl font-bold">
-        {APP_NAME}
-      </span>
-    </Link>
-  </div>
-  )
+	useEffect(() => setMounted(true), [])
+	if (!mounted) return null
+
+	const currentTheme = theme === 'system' ? systemTheme : theme
+	const logoSrc =
+		currentTheme === 'dark' ? ICONS.LOGO_DARK.path : ICONS.LOGO_LIGHT.path
+
+	return (
+		<div className="flex-start">
+			<Link href={ROUTES.HOME} className="flex-start">
+				<Image
+					src={logoSrc}
+					alt={ICONS.LOGO_LIGHT.alt}
+					width={48}
+					height={48}
+					priority
+				/>
+				<span className="hidden sm:block ml-3 text-2xl font-bold">
+					{APP_NAME}
+				</span>
+			</Link>
+		</div>
+	)
 }

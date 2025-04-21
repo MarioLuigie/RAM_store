4d083fcea54bc541b7706d6f61b864d8cb8e66e2
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-21 09:40:23 +0200

    Refactor: Change name of models to db in /lib

--- src/components/content/NewestArrivalsProducts.tsx
+++ src/components/content/NewestArrivalsProducts.tsx
@@ -1,11 +1,11 @@
 // lib
-import sampleData from '@/lib/models/sample-data'
+import sampleData from '@/lib/db/sample-data'
 // components
 import ProductList from '@/components/content/ProductList'
 
 export default function NewestArrivalsProducts() {
 	return (
-		<div className='w-full my-10'>
+		<div className="w-full my-10">
 			<h2 className="h2-bold mb-4">Newest Arrivals</h2>
 			<ProductList data={sampleData.products} />
 		</div>

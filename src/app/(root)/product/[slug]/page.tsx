dbe1c8cc80467bb8f7bc202ad291a8dad7974b79
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-23 01:07:35 +0200

    Fix: Add async await for promised params - new approach in next.js 15

--- src/app/(root)/product/[slug]/page.tsx
+++ src/app/(root)/product/[slug]/page.tsx
@@ -1,11 +1,23 @@
 import ProductDetailsPage from '@/components/pages/ProductDetailsPage'
 
-export default async function Page(props: {
-  params: Promise<{ slug: string }>
+export default async function Page({
+	params,
+}: {
+	params: Promise<{ slug: string }>
 }) {
-  const { slug } = await props.params
-  console.log("SLUG", slug)
-  return (
-    <ProductDetailsPage slug={slug} />
-  )
+	const { slug } = await params
+	return <ProductDetailsPage slug={slug} />
 }
+
+// NEXT.JS 14 - OLD APRAUCH - PARAMS IS NOT A PROMISE
+// export default async function Page({
+//   params,
+// }: {
+//   params: { slug: string }
+// }) {
+//   const { slug } = params
+//   console.log("SLUG", slug)
+
+//   return <ProductDetailsPage slug={slug} />
+// }
+// Server   Error: Route "/product/[slug]" used `params.slug`. `params` should be awaited before using its properties. Learn more:

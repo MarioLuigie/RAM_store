7c249794577557913cfab0e5a3da0d4c18de31d8
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-23 14:31:28 +0200

    Feat: Create images column, details column and action column in ProductDetailsPage.tsx

--- src/components/pages/ProductDetailsPage.tsx
+++ src/components/pages/ProductDetailsPage.tsx
@@ -37,16 +37,55 @@ export default async function ProductDetailsPage({ slug }: { slug: string }) {
 								{product.brand} {product.category}
 							</p>
 							<h1 className="h3-bold">{product.name}</h1>
-							<p>{product.rating} of {product.numReviews} Reviews</p>
+							<p>
+								{product.rating} of {product.numReviews} Reviews
+							</p>
+
+							{/* PRODUCT PRICE */}
+							<div className="flex flex-col sm:flex-row sm:items-center gap-3">
+								<ProductPrice product={product} />
+							</div>
 						</div>
+
+						<div className="mt-10">
+							<p className="font-semibold">Description</p>
+							<p>{product.description}</p>
+						</div>
+					</div>
+
+					{/* ACTION COL */}
+					<div>
+						<Card>
+							<CardContent className="p-4">
+								<div className="mb-2 flex justify-between">
+									<div>Price</div>
+									<div>
+										<ProductPrice product={product} />
+									</div>
+								</div>
+
+								<div className="mb-2 flex justify-between">
+									<div>Status</div>
+									{product.stock > 0 ? (
+										<Badge variant="outline">In Stock</Badge>
+									) : (
+										<Badge variant="destructive">Out of Stock</Badge>
+									)}
+								</div>
+								{product.stock > 0 && (
+									<div className="flex-center">
+										<Button className='w-full'>Add to Cart</Button>
+									</div>
+								)}
+							</CardContent>
+						</Card>
 					</div>
 				</div>
 			</section>
 
 			<p>{product.name}</p>
 			<p>{product.stock}</p>
-			{/* PRODUCT PRICE */}
-			<ProductPrice product={product} />
+
 			<p>{product.slug}</p>
 			<Badge variant="outline">Badge</Badge>
 		</>

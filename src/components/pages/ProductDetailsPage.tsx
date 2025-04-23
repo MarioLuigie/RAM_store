2d268a1eab43fb0ad1de0229a5b9e4bd5418a6c3
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-23 11:14:27 +0200

    Docs: Install badge shadcn component

--- src/components/pages/ProductDetailsPage.tsx
+++ src/components/pages/ProductDetailsPage.tsx
@@ -3,6 +3,9 @@ import { notFound } from 'next/navigation'
 // lib
 import { getProductBySlug } from '@/lib/actions/product.actions'
 import { Product } from '@/lib/types/products.types'
+// components
+import { Badge } from "@/components/ui/badge"
+
 
 export default async function ProductDetailsPage({ slug }: { slug: string }) {
 	const { success, data }: IDataResult<Product> = await getProductBySlug(slug)
@@ -15,6 +18,7 @@ export default async function ProductDetailsPage({ slug }: { slug: string }) {
 			<p>{data.stock}</p>
 			<p>{data.price}</p>
 			<p>{data.slug}</p>
+			<Badge variant="outline">Badge</Badge>
 		</div>
 	)
 }

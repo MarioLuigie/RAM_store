2345a51787d58ee68f4b280bc765f4913b5f6858
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-23 11:09:07 +0200

    Refactor: Add ts type for data returned by getProductBySlug server action in ProductDetailsPage.tsx

--- src/components/pages/ProductDetailsPage.tsx
+++ src/components/pages/ProductDetailsPage.tsx
@@ -1,8 +1,11 @@
-import { getProductBySlug } from '@/lib/actions/product.actions'
+// modules
 import { notFound } from 'next/navigation'
+// lib
+import { getProductBySlug } from '@/lib/actions/product.actions'
+import { Product } from '@/lib/types/products.types'
 
 export default async function ProductDetailsPage({ slug }: { slug: string }) {
-	const { success, data } = await getProductBySlug(slug)
+	const { success, data }: IDataResult<Product> = await getProductBySlug(slug)
 
 	if(!success) notFound()
 

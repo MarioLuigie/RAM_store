5e9a55f4128b9ebb21b3b1d85b79960eb14ed1a8
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-23 11:05:00 +0200

    Feat: Add getProductBySlug server action into ProductDetailsPage.tsx

--- src/components/pages/ProductDetailsPage.tsx
+++ src/components/pages/ProductDetailsPage.tsx
@@ -1,3 +1,17 @@
-export default function ProductDetailsPage({ slug }: { slug: string }) {
-	return <div>{slug}</div>
+import { getProductBySlug } from '@/lib/actions/product.actions'
+import { notFound } from 'next/navigation'
+
+export default async function ProductDetailsPage({ slug }: { slug: string }) {
+	const { success, data } = await getProductBySlug(slug)
+
+	if(!success) notFound()
+
+	return (
+		<div>
+			<p>{data.name}</p>
+			<p>{data.stock}</p>
+			<p>{data.price}</p>
+			<p>{data.slug}</p>
+		</div>
+	)
 }

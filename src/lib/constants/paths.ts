4608a5806cd32cd1ced96be396a73d62416172c8
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-24 13:16:46 +0200

    Refactor: Order code in paths.ts

--- src/lib/constants/paths.ts
+++ src/lib/constants/paths.ts
@@ -1,14 +1,13 @@
 export const ROUTES = {
-  HOME: '/',
-  PRODUCT: '/product',
-  CART: '/cart',
-  SIGN_IN: '/sign-in',
-  SIGN_OUT: '/sign-out',
+	HOME: '/',
+	PRODUCT: '/product',
+	CART: '/cart',
+	SIGN_IN: '/sign-in',
+	SIGN_OUT: '/sign-out',
 }
 
 export const ICONS_PATH = {
-  LOGO_LIGHT: '/assets/icons/logo-light-mode.svg',
-  LOGO_DARK: '/assets/icons/logo-dark-mode.svg',
-    LOADER: '/assets/icons/loader.gif',
+	LOGO_LIGHT: '/assets/icons/logo-light-mode.svg',
+	LOGO_DARK: '/assets/icons/logo-dark-mode.svg',
+	LOADER: '/assets/icons/loader.gif',
 }
-

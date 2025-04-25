5b96688ba514c9cde82b262abb944c5614b8f46c
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-25 15:47:42 +0200

    Feat: Create signInUserWithCredentials and signOutUser in user.actions.ts

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -1,2 +1,43 @@
 'use server'
+// modules
+import { isRedirectError } from 'next/dist/client/components/redirect-error'
+// lib
+import { SignInFormSchema } from '@/lib/utils/validators'
+import { signIn, signOut } from '@/config/auth'
 
+// SIGN IN THE USER WITH CREDENTIALS
+
+export async function SignInUserWithCredentials(
+	prevState: unknown,
+	formData: FormData
+) {
+	try {
+		const user = SignInFormSchema.parse({
+			email: formData.get('email'),
+			password: formData.get('password'),
+		})
+
+		await signIn('credentials', user)
+
+		return {
+			success: true,
+			message: 'Signed in successfully',
+		}
+	} catch (error: unknown) {
+		if (isRedirectError(error)) {
+			throw error
+		} else {
+			console.error('Unknown error:', error)
+		}
+
+		return {
+			success: false,
+			message: 'Invalid e-mail or password',
+		}
+	}
+}
+
+// SIGN USER OUT
+export async function signOutUser() {
+	await signOut()
+}

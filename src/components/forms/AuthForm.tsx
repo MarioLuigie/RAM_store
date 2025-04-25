7aad6bf298b028b79bd1b5fa5fded8a2ce0b847e
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-25 21:54:15 +0200

    Fix: Fix hydration error from SignInPage.tsx - anchor inside anchor

--- src/components/forms/AuthForm.tsx
+++ src/components/forms/AuthForm.tsx
@@ -1,10 +1,40 @@
 'use client'
+// lib
+import { SIGNIN_DEFAULT_VALUES } from '@/lib/constants'
+// components
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 
 export default function AuthForm() {
+	return (
+		<form className="bg-yellow-100">
+			<div className="space-y-6">
+				{/* EMAIL INPUT */}
+				<div>
+					<Label htmlFor="email">E-mail</Label>
+					<Input
+						id="email"
+						name="email"
+						type="email"
+						required
+						autoComplete="email"
+						defaultValue={SIGNIN_DEFAULT_VALUES.email}
+					/>
+				</div>
 
-  return (
-    <div className="bg-yellow-100">
-      AUTH FORM
-    </div>
-  )
+				{/* PASSWORD INPUT */}
+				<div>
+					<Label htmlFor="password">Password</Label>
+					<Input
+						id="password"
+						name="password"
+						type="password"
+						required
+						autoComplete="password"
+						defaultValue={SIGNIN_DEFAULT_VALUES.password}
+					/>
+				</div>
+			</div>
+		</form>
+	)
 }

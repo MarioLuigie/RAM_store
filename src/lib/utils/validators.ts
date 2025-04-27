7d02cac9407f0fc6842b25bbe296bee9b50a7f1d
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-27 22:17:13 +0200

    Feat: Create cart and cart item schema in validators.ts

--- src/lib/utils/validators.ts
+++ src/lib/utils/validators.ts
@@ -1,11 +1,50 @@
 import { z } from 'zod'
 import { formatNumberWithDecimalToString } from '@/lib/utils/utils'
 
+// SCHEMA FOR SIGN IN FORM
+export const SignInFormSchema = z
+	.object({
+		email: z
+			.string({ required_error: 'E-mail is required' })
+			.email('Enter a valid e-mail address')
+			.transform((val) => val.trim().toLowerCase()),
+		password: z
+			.string({ required_error: 'Password is required' })
+			.min(6, 'Password must be at least 6 characters')
+			.max(100, 'Password is too long'),
+	})
+	.strict() // odrzuca nieznane pola, zapobiega atakom i błędom
+
+// SCHEMA FOR SIGN UP FORM
+export const SignUpFormSchema = z
+	.object({
+		name: z.string().min(3, 'Name must be at least 3 characters'),
+		email: z
+			.string({ required_error: 'E-mail is required' })
+			.email('Enter a valid e-mail address')
+			.transform((val) => val.trim().toLowerCase()),
+		password: z
+			.string({ required_error: 'Password is required' })
+			.min(6, 'Password must be at least 6 characters')
+			.max(100, 'Password is too long'),
+		confirmPassword: z
+			.string()
+			.min(6, 'Confirm Password must be at least 6 characters')
+			.max(100, 'Password is too long'),
+	})
+	.strict()
+	.refine((data) => data.password === data.confirmPassword, {
+		message: 'Password and Confirm Password do not match',
+		path: ['confirmPassword'],
+	})
+
 // SCHEMA FOR INSERTING PRODUCTS
 const currency = z
 	.string()
-	.refine((value) =>
-		/^\d+(\.\d{2})?$/.test(formatNumberWithDecimalToString(Number(value)))
+	.refine(
+		(value) =>
+			/^\d+(\.\d{2})?$/.test(formatNumberWithDecimalToString(Number(value))),
+		'Price must have exactly two decimal places'
 	)
 
 export const ProductSchema = z.object({
@@ -23,39 +62,23 @@ export const ProductSchema = z.object({
 	numReviews: z.string(),
 })
 
-// SCHEMA FOR SIGN IN FORM
-export const SignInFormSchema = z
-	.object({
-		email: z
-			.string({ required_error: 'E-mail is required' })
-			.email('Enter a valid e-mail address')
-			.transform((val) => val.trim().toLowerCase()),
-		password: z
-			.string({ required_error: 'Password is required' })
-			.min(6, 'Password must be at least 6 characters')
-			.max(100, 'Password is too long'),
-	})
-	.strict() // odrzuca nieznane pola, zapobiega atakom i błędom
-
-	// SCHEMA FOR SIGN UP FORM
-export const SignUpFormSchema = z
-.object({
-	name: z
-		.string().min(3, 'Name must be at least 3 characters'),
-	email: z
-		.string({ required_error: 'E-mail is required' })
-		.email('Enter a valid e-mail address')
-		.transform((val) => val.trim().toLowerCase()),
-	password: z
-		.string({ required_error: 'Password is required' })
-		.min(6, 'Password must be at least 6 characters')
-		.max(100, 'Password is too long'),
-	confirmPassword: z
-		.string().min(6, 'Confirm Password must be at least 6 characters')
-		.max(100, 'Password is too long')
+// SCHEMA FOR CART ITEM
+export const CartItemSchema = z.object({
+	productId: z.string().min(1, 'Product is required'),
+	name: z.string().min(1, 'Name is required'),
+	slug: z.string().min(1, 'Slug is required'),
+	qty: z.number().int().nonnegative('Quantity must be a possitive number'),
+	image: z.string().min(1, 'Image is required'),
+	price: currency,
 })
-.strict() 
-.refine((data) => data.password === data.confirmPassword, {
-	message: 'Password and Confirm Password do not match',
-	path: ['confirmPassword'],
+
+// SCHEMA FOR CART
+export const CartSchema = z.object({
+	items: z.array(CartItemSchema),
+	itemsPrice: currency,
+	shippingPrice: currency,
+	taxPrice: currency,
+	totalPrice: currency,
+	sessionCartId: z.string().min(1, 'Session cart id is required'),
+	userId: z.string().optional().nullable(),
 })

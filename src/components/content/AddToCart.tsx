a015807e600f9f9370017fa8872ac4e43df1c2ee
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-27 23:03:22 +0200

    Feat: Create AddToCart.tsx

---
+++ src/components/content/AddToCart.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import { CartItem } from '@/lib/types/cart.types'
+
+export default function AddToCart({ item }: { item: CartItem }) {
+	console.log(item)
+	return <div>ADD TO CART</div>
+}

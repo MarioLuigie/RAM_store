3f6647b9ddcefb92821b8edd8518d6bc7918c965
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-29 21:18:26 +0200

    Test: Add cookie value reading from request header and from cookies() only for tests

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -7,8 +7,9 @@ import AuthPage from '@/components/pages/AuthPage'
 
 // Only for tests - read my ram-session-cart-id cookie (from requst headers added in middleware) in SSR server component
 // read cookie from cookies() non possible in server components
-import { headers } from 'next/headers'
 import { SESSION_CART_ID } from '@/lib/constants'
+import { headers } from 'next/headers'
+import { cookies } from 'next/headers'
 
 export const metadata: Metadata = {
 	title: 'Sign in',
@@ -21,8 +22,13 @@ export default async function Page({
 }) {
 	const { callbackUrl } = await searchParams
 
+	// TEST - read cookie value from request headers (nesecery in API routes ex.)
 	const sessionCartId = (await headers()).get(SESSION_CART_ID)
-	console.log("COOKIE PAYLOAD FROM MIDDLEWARE", sessionCartId)
+	console.log('COOKIE PAYLOAD FROM MIDDLEWARE', sessionCartId)
+
+	// TEST - read cookie value from cookies()
+	const cookie = (await cookies()).get(SESSION_CART_ID)
+	console.log('COOKIE PAYLOAD FROM cookies()', cookie?.value)
 
 	return <AuthPage callbackUrl={callbackUrl} type={AuthTypes.SIGN_IN} />
 }

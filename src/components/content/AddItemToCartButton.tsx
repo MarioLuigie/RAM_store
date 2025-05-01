a83264760989b99908353c2002c16f5237dba9c6
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-05-02 01:40:35 +0200

    Styles: Add min-w for dynamic ad to cart button in AddItemToCartButton.tsx

--- src/components/content/AddItemToCartButton.tsx
+++ src/components/content/AddItemToCartButton.tsx
@@ -22,17 +22,29 @@ export default function AddItemToCartButton({
 		cart && cart.items.find((item) => item.productId === cartItem.productId);
 
 	return existCartItem ? (
-		<div className="w-full mt-4">
+		<div className="flex items-center justify-between mt-4">
 			<Button
 				type="button"
 				variant='outline'
-				className="w-full cursor-pointer"
+				className="cursor-pointer"
 				onClick={() =>
 					handleRemoveItemFromCart(existCartItem.productId, showRemovedItemFromCartToast)
 				}
 			>
-				<Minus />
-				Remove from Cart
+				<Minus className='h-4 w-4'/>
+			</Button>
+			<span className='min-w-12 w-full flex-center text-xl px-2'>
+				{existCartItem.qty}
+			</span>
+			<Button
+				type="button"
+				variant='outline'
+				className="cursor-pointer"
+				onClick={() =>
+					handleAddItemToCart(cartItem, showAddItemToCartToast)
+				}
+			>
+				<Plus className='h-4 w-4'/>
 			</Button>
 		</div>
 	) : (

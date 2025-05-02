73aba05a59acf4af4de592eb6daecc62b9f47424
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-05-02 02:02:09 +0200

    Feat: Create CartPage.tsx and import to /cart/page

--- src/app/(root)/cart/page.tsx
+++ src/app/(root)/cart/page.tsx
@@ -1,5 +1,7 @@
 // modules
 import { Metadata } from 'next'
+// components
+import CartPage from '@/components/pages/CartPage'
 
 export const metadata: Metadata = {
 	title: 'Cart',
@@ -8,8 +10,6 @@ export const metadata: Metadata = {
 export default function Page() {
 
   return (
-    <div>
-      CART
-    </div>
+    <CartPage />
   )
 }

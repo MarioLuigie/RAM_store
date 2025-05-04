7467ce90050c9a9e0c61329dfddad24e36895991
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-05-04 13:29:39 +0200

    Feat: Add removing or adding products buttons into CartTable.tsx

--- src/components/content/CartTable.tsx
+++ src/components/content/CartTable.tsx
@@ -14,9 +14,16 @@ import {
 import Link from 'next/link';
 import Image from 'next/image';
 import { ROUTES } from '@/lib/constants/paths';
+import { Button } from '../ui/button';
+import { Minus, Plus } from 'lucide-react';
+import { handleAddItemToCart, handleRemoveItemFromCart } from '@/lib/handlers/cart.handlers';
+import { useAddItemToCartToast } from '@/lib/hooks/useAddItemToCartToast';
+import { useRemovedItemFromCartToast } from '@/lib/hooks/useRemovedItemFromCartToast';
 
 export default function CartTable({ cart }: { cart?: Cart }) {
 	console.log(cart);
+		const { showAddItemToCartToast } = useAddItemToCartToast();
+		const { showRemovedItemFromCartToast } = useRemovedItemFromCartToast();
 
 	return (
 		<div className="grid md:grid-cols-4 md:gap-5">
@@ -27,7 +34,7 @@ export default function CartTable({ cart }: { cart?: Cart }) {
 					</TableCaption>
 					<TableHeader>
 						<TableRow>
-							<TableHead className="w-[100px]">Product</TableHead>
+							<TableHead>Product</TableHead>
 							<TableHead className="text-center">Quantity</TableHead>
 							<TableHead className="text-right">Price</TableHead>
 						</TableRow>
@@ -36,19 +43,47 @@ export default function CartTable({ cart }: { cart?: Cart }) {
 						{cart?.items?.map((item) => (
 							<TableRow key={item.slug}>
 								<TableCell className="font-medium">
-                  <Link href={`${ROUTES.PRODUCT}/${item.slug}`} className='flex items-center gap-5'>
-                    <Image 
-                      src={item.image}
-                      alt={item.name}
-                      className="w-[45px] h-[45px] flex-shrink-0 rounded-sm"
-                      width={45}
-                      height={45}
-                    />
-                    <p>{item.name}</p>
-                  </Link>
-                </TableCell>
-								<TableCell className='text-center'>{item.qty}</TableCell>
-								<TableCell className="text-right">{`$${item.price}`}</TableCell>
+									<Link
+										href={`${ROUTES.PRODUCT}/${item.slug}`}
+										className="flex items-center gap-5"
+									>
+										<Image
+											src={item.image}
+											alt={item.name}
+											className="w-[45px] h-[45px] flex-shrink-0 rounded-sm"
+											width={45}
+											height={45}
+										/>
+										<p>{item.name}</p>
+									</Link>
+								</TableCell>
+								<TableCell className="flex-center">
+									{/* CHANGE QUANTITY OF ITEMS IN CART - ADDING OR REMOVING */}
+									<div className="flex items-center justify-between w-[140px]">
+										<Button
+											disabled={false}
+											type="button"
+											variant="outline"
+											className="cursor-pointer"
+											onClick={() => handleRemoveItemFromCart(item.productId, showRemovedItemFromCartToast)}
+										>
+											<Minus className="h-4 w-4" />
+										</Button>
+										<span className="min-w-12 w-full flex-center text-xl px-2">
+											{item.qty || '0'}
+										</span>
+										<Button
+											disabled={false}
+											type="button"
+											variant="outline"
+											className="cursor-pointer"
+											onClick={() => handleAddItemToCart(item, showAddItemToCartToast)}
+										>
+											<Plus className="h-4 w-4" />
+										</Button>
+									</div>
+								</TableCell>
+								<TableCell className="text-right">{`$${Number(item.price) * item.qty}`}</TableCell>
 							</TableRow>
 						))}
 					</TableBody>

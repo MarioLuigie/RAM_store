8f3a06ad5759215b5256d2fcc887b7522c3ac7da
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-05-05 12:01:27 +0200

    Feat: Create ShippingAddressSchema in validators.ts and ShippingAddress type in shipping.types.ts

---
+++ src/lib/types/shipping.types.ts
@@ -0,0 +1,4 @@
+import { z } from 'zod'
+import { ShippingAddressSchema } from '@/lib/utils/validators'
+
+export type ShippingAddress = z.infer<typeof ShippingAddressSchema>
--- src/lib/utils/validators.ts
+++ src/lib/utils/validators.ts
@@ -82,3 +82,13 @@ export const CartSchema = z.object({
 	taxPrice: currency,
 	totalPrice: currency,
 })
+
+// SCHEMA FOR SHIPPING ADDRESS
+export const ShippingAddressSchema = z.object({
+	fullName: z.string().min(3, 'Name must be at least 3 characters'),
+	streetAddress: z.string().min(3, 'Street address must be at least 3 characters'),
+	city: z.string().min(3, 'City must be at least 3 characters'),
+	postalCode: z.string().min(3, 'Postal code must be at least 3 characters'),
+	lat: z.number().optional(),
+	lng: z.number().optional(),
+})

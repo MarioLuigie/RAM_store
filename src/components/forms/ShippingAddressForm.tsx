4a667a2278e8efc6f28d5bdc857e5ad708c73330
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-05-05 15:04:49 +0200

    Feat: Build shipping address form in ShippingAddressForm.tsx

--- src/components/forms/ShippingAddressForm.tsx
+++ src/components/forms/ShippingAddressForm.tsx
@@ -2,9 +2,32 @@
 // modules
 import { useRouter } from 'next/navigation';
 import { useTransition } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { ControllerRenderProps, SubmitHandler, useForm } from 'react-hook-form';
+import { z } from 'zod';
 // lib
 import { ShippingAddress } from '@/lib/types/shipping.types';
 import { ShippingAddressSchema } from '@/lib/utils/validators';
+// components
+import { Button } from '@/components/ui/button';
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { SHIPPING_ADDRESS_DEFAULT_VALUES } from '@/lib/constants';
+import Loader from '@/components/shared/Loader';
+import { ArrowRight, SendHorizontal } from 'lucide-react';
+
+type FullNameField = ControllerRenderProps<ShippingAddress, 'fullName'>;
+type StreetAddressField = ControllerRenderProps<ShippingAddress, 'streetAddress'>;
+type CityField = ControllerRenderProps<ShippingAddress, 'city'>;
+type PostalCodeField = ControllerRenderProps<ShippingAddress, 'postalCode'>;
+type CountryField = ControllerRenderProps<ShippingAddress, 'country'>;
 
 export default function ShippingAddressForm({
 	address,
@@ -13,7 +36,126 @@ export default function ShippingAddressForm({
 }) {
 	console.log(address);
 
-  const router = useRouter();
+	const router = useRouter();
 
-	return <div>SHIPPING ADDRESS FORM</div>;
+	const form = useForm<ShippingAddress>({
+		resolver: zodResolver(ShippingAddressSchema),
+		defaultValues: address || SHIPPING_ADDRESS_DEFAULT_VALUES,
+	});
+
+	const [isPending, startTransition] = useTransition();
+
+	const onSubmit: SubmitHandler<ShippingAddress> = async (
+		shippingAddressFormValues: ShippingAddress
+	) => {
+		console.log(shippingAddressFormValues);
+	};
+
+	return (
+		<div className="max-w-md mx-auto space-y-4">
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className="flex flex-col gap-4"
+				>
+					<div className="flex flex-col md:flex-row gap-5">
+						<FormField
+							control={form.control}
+							name="fullName"
+							render={({ field }: {field: FullNameField}) => (
+								<FormItem className="w-full">
+									<FormLabel>Full Name</FormLabel>
+									<FormControl>
+										<Input placeholder="Enter full name" {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+
+					<div className="flex flex-col md:flex-row gap-5">
+						<FormField
+							control={form.control}
+							name="streetAddress"
+							render={({ field }: { field: StreetAddressField}) => (
+								<FormItem className="w-full">
+									<FormLabel>Street Address</FormLabel>
+									<FormControl>
+										<Input
+											placeholder="Enter street address"
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+
+					<div className="flex flex-col md:flex-row gap-5">
+						<FormField
+							control={form.control}
+							name="city"
+							render={({ field }: { field: CityField}) => (
+								<FormItem className="w-full">
+									<FormLabel>City</FormLabel>
+									<FormControl>
+										<Input placeholder="Enter city" {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+
+					<div className="flex flex-col md:flex-row gap-5">
+						<FormField
+							control={form.control}
+							name="postalCode"
+							render={({ field }: { field: PostalCodeField}) => (
+								<FormItem className="w-full">
+									<FormLabel>Postal Code</FormLabel>
+									<FormControl>
+										<Input
+											placeholder="Enter postal code"
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+
+					<div className="flex flex-col md:flex-row gap-5">
+						<FormField
+							control={form.control}
+							name="country"
+							render={({ field }: { field: CountryField}) => (
+								<FormItem className="w-full">
+									<FormLabel>Country</FormLabel>
+									<FormControl>
+										<Input placeholder="Enter country" {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+
+					<div className="flex justify-center gap-2 w-full">
+						<Button type="submit" disabled={isPending}>
+							{isPending ? (
+								<Loader width={16} height={16} />
+							) : (
+								<SendHorizontal />
+							)}
+							Submit
+						</Button>
+					</div>
+				</form>
+			</Form>
+		</div>
+	);
 }
--- src/lib/types/shipping.types.ts
+++ src/lib/types/shipping.types.ts
@@ -2,3 +2,4 @@ import { z } from 'zod'
 import { ShippingAddressSchema } from '@/lib/utils/validators'
 
 export type ShippingAddress = z.infer<typeof ShippingAddressSchema>
+

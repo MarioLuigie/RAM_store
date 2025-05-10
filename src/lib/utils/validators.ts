3c4c0c67b7b13416d71216cb0e2401c74bbeaebd
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-05-10 09:32:25 +0200

    Feat: Create InsertOrderSchema and InsertOrderItemSchema in validators.ts

--- src/lib/utils/validators.ts
+++ src/lib/utils/validators.ts
@@ -106,3 +106,27 @@ export const PaymentMethodSchema = z
 		path: ['type'],
 		message: 'Invalid payment method',
 	});
+
+
+	// SCHEMA FOR INSERTING ORDER
+	export const insertOrderSchema = z.object({
+		userId: z.string().min(1, 'User is required'),
+		itemsPrice: currency,
+		shippingPrice: currency,
+		taxPrice: currency,
+		totalPrice: currency,
+		paymentMethod: z.string().refine((data) => PAYMENT_METHODS.includes(data), {
+			message: 'Invalid payment method'
+		}),
+		shippingAddress: ShippingAddressSchema,
+	});
+
+	// SCHEMA FOR INSERTING ORDER ITEM
+	export const insertOrderItemSchema = z.object({
+		productId: z.string(),
+		slug: z.string(),
+		image: z.string(),
+		name: z.string(),
+		price: currency,
+		qty: z.number(),
+	});

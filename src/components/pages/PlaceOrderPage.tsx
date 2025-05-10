8cf11b0d1c2376b6145c75a336400c47d4471b2e
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-05-10 10:39:15 +0200

    Feat: Prepare cart, address and payment method in PlaceOrderPage.tsx

--- src/components/pages/PlaceOrderPage.tsx
+++ src/components/pages/PlaceOrderPage.tsx
@@ -1,7 +1,25 @@
+// lib
+import { getCart } from '@/lib/actions/cart.actions';
+import { auth } from '@/config/auth';
 // components
 import CheckoutSteps from '@/components/shared/CheckoutSteps';
+import { getUserById } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
+import { ROUTES } from '@/lib/constants/paths';
 
-export default function PlaceOrder() {
+export default async function PlaceOrderPage() {
+	const cart = await getCart();
+	const session = await auth();
+	const userId = session?.user?.id;
+
+	if (!userId) throw new Error('User not found');
+
+	const { data: user } = await getUserById(userId);
+
+	if (!cart || cart.items.length === 0) redirect(ROUTES.CART);
+	if (!user.address) redirect(ROUTES.SHIPPING_ADDRESS);
+	if (!user.paymentMethod) redirect(ROUTES.PAYMENT_METHOD)
+
 	return (
 		<>
 			<CheckoutSteps current={3} />

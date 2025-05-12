c738580447651130f5a655616ec476955459122a
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-05-12 10:01:33 +0200

    Feat: Rebuild paypal in paypal.ts

--- src/lib/payments/paypal.test.ts
+++ src/lib/payments/paypal.test.ts
@@ -1,9 +1,12 @@
+import { CURRENCY_CODES } from '../constants/index';
 import { generateAccessToken } from './paypal';
 
 // TEST TO GENERATE ACCESS TOKEN FROM PAYPAL
 test('generate token from paypal', async () => {
 	const tokenRes = await generateAccessToken();
 	console.log(tokenRes);
+	console.log(CURRENCY_CODES.main);
 	expect(typeof tokenRes).toBe('string');
 	expect(tokenRes.length).toBeGreaterThan(0);
+	expect(typeof CURRENCY_CODES.main).toBe('string');
 });
--- src/lib/payments/paypal.ts
+++ src/lib/payments/paypal.ts
@@ -1,9 +1,42 @@
+import { CURRENCY_CODES } from '@/lib/constants';
+
 const base = process.env.PAYPAL_API_URL || 'https://api-m.sandbox.paypal.com';
 
-export const paypal = {};
+export const paypal = {
+	createOrder: async (price: number) => {
+		const accessToken = await generateAccessToken();
+		const url = `${base}/v2/checkout/orders`;
+
+		const res = await fetch(url, {
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer ${accessToken}`,
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				intent: 'CAPTURE',
+				purchase_units: [
+					{
+						amount: {
+							currency_code: CURRENCY_CODES.main,
+							value: price,
+						},
+					},
+				],
+			}),
+		});
+
+		if (res.ok) {
+			return await res.json();
+		} else {
+			const errorMessage = await res.text();
+			throw new Error(errorMessage);
+		}
+	},
+};
 
 // GENERATE PAYPAL ACCESS TOKEN
-export async function generateAccessToken() {
+async function generateAccessToken() {
 	const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
 
 	const auth = Buffer.from(
@@ -19,11 +52,22 @@ export async function generateAccessToken() {
 		},
 	});
 
-  if (res.ok) {
-    const data = await res.json();
-    return data.access_token;
-  } else {
-    const errorMessage = await res.text();
-    throw new Error(errorMessage)
-  }
+	if (res.ok) {
+		const data = await res.json();
+		return data.access_token;
+	} else {
+		const errorMessage = await res.text();
+		throw new Error(errorMessage);
+	}
 }
+
+// async function handleResponse(res: Response) {
+// 	if (res.ok) {
+// 		return res.json();
+// 	} else {
+// 		const errorMessage = await res.text();
+// 		throw new Error(errorMessage);
+// 	}
+// }
+
+export { generateAccessToken };

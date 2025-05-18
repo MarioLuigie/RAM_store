5368967d6202b5fce0b564e7f234e3349dd1f654
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-05-18 14:19:21 +0200

    Feat: Increase elastic Header component adding children as a prop

---
+++ src/app/user/layout.tsx
@@ -0,0 +1,9 @@
+import { APP_NAME } from '@/lib/constants';
+
+export default function UserLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return <>{children} USER LAYOUT</>;
+}
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,13 +3,20 @@ import Logo from '@/components/shared/Logo';
 import Menu from '@/components/shared/Menu';
 import { getCart } from '@/lib/actions/cart.actions';
 
-export default async function Header() {
+export default async function Header({
+	children,
+}: {
+	children?: React.ReactNode;
+}) {
 	const cart = await getCart();
 
 	return (
 		<header className="w-full border-b">
 			<div className="wrapper flex-between">
-				<Logo />
+				<div className='flex items-center gap-4'>
+					<Logo />
+					{children}
+				</div>
 				<Menu cartItems={cart?.items || []} />
 			</div>
 		</header>

69c974eda41b39530f740bd9355b068b108674f3
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-05-24 14:46:00 +0200

    Feat: Get all orders in AdminOrdersPage.tsx

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -3,15 +3,18 @@ import { Metadata } from 'next';
 // lib
 import { APP_ROUTE_NAME_ORDERS } from '@/lib/constants';
 // components
-import AdminOrdersPage from "@/components/pages/admin/AdminOrdersPage";
+import AdminOrdersPage from '@/components/pages/admin/AdminOrdersPage';
 
 export const metadata: Metadata = {
-  title: `Admin ${APP_ROUTE_NAME_ORDERS}`,
+	title: `Admin ${APP_ROUTE_NAME_ORDERS}`,
 };
 
-export default function Page() {
+export default async function Page({
+	searchParams,
+}: {
+	searchParams: Promise<{ page: string }>;
+}) {
+  const { page } = await searchParams;
 
-  return (
-    <AdminOrdersPage />
-  )
+	return <AdminOrdersPage page={page} />;
 }
--- src/components/pages/admin/AdminOrdersPage.tsx
+++ src/components/pages/admin/AdminOrdersPage.tsx
@@ -1,7 +1,15 @@
 import { requireAdmin } from "@/lib/utils/auth-guard";
+import { getAllOrders } from "@/lib/actions/order.actions";
 
-export default async function AdminOrdersPage() {
+export default async function AdminOrdersPage({
+  page = '1'
+}: {
+  page: string
+}) {
   await requireAdmin();
+
+  const { success, data, message } = await getAllOrders({limit: 2, page: Number(page)});
+
   return (
     <div>
       ORDERS PAGE

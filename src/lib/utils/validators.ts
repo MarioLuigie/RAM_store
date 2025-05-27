863974a1696f027c6ea66a16f23f59a25664eafc
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-05-27 21:39:27 +0200

    Feat: Create AddProductSchema in validators.ts

--- src/lib/utils/validators.ts
+++ src/lib/utils/validators.ts
@@ -144,3 +144,17 @@ export const UpdateProfileSchema = z.object({
 	name: z.string().min(3, 'Name must be at least 3 characters'),
 	email: z.string().min(3, 'E-mail must be at least 3 characters'),
 });
+
+// SCHEMA FOR INSERTING/ADDING PRODUCTS
+export const AddProductSchema = z.object({
+	name: z.string().min(3, 'Name must be at least 3 characters'),
+	slug: z.string().min(3, 'Slug must be at least 3 characters'),
+	category: z.string().min(3, 'Category must be at least 3 characters'),
+	brand: z.string().min(3, 'Brand must be at least 3 characters'),
+	description: z.string().min(3, 'Description must be at least 3 characters'),
+	stock: z.coerce.number(),
+	images: z.array(z.string()).min(1, 'Product must have at least one image'),
+	isFeatured: z.boolean(),
+	banner: z.string().nullable(),
+	price: currency,
+});

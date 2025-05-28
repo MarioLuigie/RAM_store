a7c948aebfbdb347a4d48e483d9932271b9e78bd
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-05-28 14:56:54 +0200

    Feat: Rebuild NotFoundPage and add isAdmin condicinal

--- src/components/pages/NotFoundPage.tsx
+++ src/components/pages/NotFoundPage.tsx
@@ -1,15 +1,18 @@
 // lib
 import { ROUTES } from '@/lib/constants/paths';
+import { checkIsAdmin } from '@/lib/utils/auth-guard';
 // components
 import RedirectCard from '@/components/shared/RedirectCard';
 
-export default function NotFoundPage() {
+export default async function NotFoundPage() {
+	const { isAdmin } = await checkIsAdmin();
+
 	return (
 		<RedirectCard
 			title="Not Found"
 			desc="Could not find requested resource"
-			redirectPath={ROUTES.HOME}
-			redirectLabel="Back to Home"
+			redirectPath={isAdmin ? ROUTES.ADMIN_OVERVIEW : ROUTES.HOME}
+			redirectLabel={isAdmin ? "Back to Admin Dashboard" : "Back to Home"}
 		/>
 	);
 }

b4df50f4382a1c71ea0553aa872d828f84543b01
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-06-22 22:00:47 +0200

    Feat: Create queries object and inject them to the SearchParams.tsx

--- src/app/(root)/search/page.tsx
+++ src/app/(root)/search/page.tsx
@@ -3,9 +3,32 @@ import SearchPage from '@/components/pages/SearchPage';
 export default async function Page({
 	searchParams,
 }: {
-	searchParams: Promise<{ category: string; query: string }>;
+	searchParams: Promise<{
+		category?: string;
+		query?: string;
+		price?: string;
+		rating?: string;
+		sort?: string;
+		page?: string;
+	}>;
 }) {
-	const { category, query } = await searchParams;
+	const {
+		category = 'all',
+		query = 'all',
+		price = 'all',
+		rating = 'all',
+		sort = 'newest',
+		page = '1',
+	} = await searchParams;
 
-	return <SearchPage category={category} query={query} />;
+  const queries = {
+    category,
+    query,
+    price,
+    rating,
+    sort,
+    page,
+  }
+
+	return <SearchPage queries={queries} />;
 }
--- src/components/pages/SearchPage.tsx
+++ src/components/pages/SearchPage.tsx
@@ -1,9 +1,35 @@
-export default function SearchPage({
-	category,
-	query,
-}: {
+// lib
+import { getProducts } from '@/lib/actions/product.actions';
+
+type QueriesProps = {
 	category: string;
 	query: string;
+	price: string;
+	rating: string;
+	sort: string;
+	page: string;
+};
+
+export default async function SearchPage({
+	queries,
+}: {
+	queries: QueriesProps;
 }) {
-	return <div>SEARCH PAGE{' '}{category}{' '}{query}</div>;
+
+	const { success, data } = await getProducts({
+		category: queries.category,
+		query: queries.query,
+		page: Number(queries.page),
+	});
+
+	if (!success || !data) throw new Error('Products not found');
+  
+	return (
+		<div>
+			SEARCH PAGE{' '}
+			{Object.values(queries).map((query, i) => (
+				<p key={i}>{query}</p>
+			))}
+		</div>
+	);
 }
